3f5c780dbb893dd36faf56de910e2e905b72da4b
백상휘 <sanghwiback@gmail.com>
2022-03-21 11:01:34 +0900

    Initial Project commit
    프로젝트 최초 생성 후 불필요한 주석을 제거하였습니다.
    작업의 초기 소스코드로 주석 제거 이외에 아무 작업도 하지 않았습니다.

---
+++ Photos/Photos/SceneDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  SceneDelegate.swift
+//  Photos
+//
+//  Created by 백상휘 on 2022/03/21.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+}
+
---
+++ Photos/Photos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Photos
+//
+//  Created by 백상휘 on 2022/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
